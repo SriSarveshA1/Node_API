171b957c108075908ae0341007e0b2882476a795
srisarvesh <r.srisarvesh@gmail.com>
2021-07-21 19:04:24 +0530

    After connecting database using mongoose

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express=require('express');
 const app=express();
 const postRoutes=require('./routes/post');
+const mongoose=require('mongoose');
+const dotenv=require('dotenv');
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true }).then(()=>console.log("DB Connected"));//we pass { useNewUrlParser: true } to connect to get rid of deprecation warnings.
+
+mongoose.connection.on("error",(err)=>{
+    console.log(err.message);
+})
+
 const morgan=require('morgan');
 app.use(morgan("dev"));
 app.get('/',postRoutes);
 
 
-const port =3000;
+const port =process.env.PORT||3000;//So either in production environment we set the port(in .env file we set the port) or default 3000 is the port
 app.listen(port,()=>{
     console.log("sds");
 });
--- controllers/post.js
+++ controllers/post.js
@@ -1,7 +1,8 @@
 const getPost=(req,res)=>{
-  res.json({
-    post:[{title:'1'},{title:'2'},{title:'3'}]
-       }
-    );
-}
-module.exports={getPost};
+    res.send("Hlo from the functionality of the post from the controllers");
+    res.json({
+      post:[{title:'1'},{title:'2'},{title:'3'}]
+         }
+      );
+  }
+  module.exports={getPost};
